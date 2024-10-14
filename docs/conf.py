a0f89ce09667d84597b8907b5eec02b6d4cab0e5
Alpumutk <79879109+Alpumutk@users.noreply.github.com>
2024-10-14 15:36:59 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,13 +24,16 @@ author = 'Alp Kurbay, Brian Robinson, Andre Schleife'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+# Build PDF & ePub
+formats:
+  - epub
+  - pdf
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-formats: all
 
 extensions = [
     'sphinx.ext.autodoc',         # Automatically document Python modules
